7d240c6318e182ffc9a97b0b9bc49ecce95a9c25
NicoBral <nicolas.bral@gmail.com>
2016-10-19 15:02:18 +0200

    version optimisee du script

--- script1.m
+++ script1.m
@@ -1,21 +1,21 @@
 
-    for k = 1:1
-    % Load an image into Matlab
-      pic = imread('image/westerncelars.JPG');
+      
+      str = 'images/westerncelars.JPG'
+      pic = imread(str);
       imshow(pic);
         
       
       
-      info = imfinfo('image/westerncelars.JPG');
+      info = imfinfo(str);
       info.DigitalCamera;
       f=info.DigitalCamera. FocalLengthIn35mmFilm;
  
       
-      num_input = 1; 
+     
       clearvars c_info
-      for j = 1:num_input
+   
       % select n number of data points using the data cursor - in this case
-      % 4
+      % 4 (points ABCD)
           for i = 1:4
               shg
               dcm_obj = datacursormode(1);
@@ -24,20 +24,18 @@
               waitforbuttonpress
               c_info{i,j} = getCursorInfo(dcm_obj);
           end
-      end
+      
     % Export the positions obtained by the data cursor to the workspace
-      for j = 1:num_input
+      
           for i = 1:size(c_info,1)
               positions{i,j} = c_info{i,j}.Position;
-          end
+         
       end
-      for i = 1:num_input
-          % some code that fits your spline function to the data 
-      end
+     
       close all
       
     
-    end
+
     
     
  
