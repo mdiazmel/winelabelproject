a4b1607337456f9ed22073cc9e366d32ac22acf2
NicoBral <nicolas.bral@gmail.com>
2016-11-15 12:46:49 +0100

    version optimisee du calcul de l'intersection

---
+++ plotintersections.m
@@ -0,0 +1,31 @@
+
+
+intersection_1;
+intersection_2;
+
+nRow = 1;
+nCol = 1;
+n = nRow*nCol;
+
+
+figure(1); clf;
+for i=1:nRow
+    for j=1:nCol
+        idx = nCol*(i-1) + j;
+        subplot(nRow,nCol,idx); hold on;
+        plot([A(1,idx), B(1,idx)],[A(2,idx), B(2,idx)],'k-','LineWidth',2);
+        plot([C(1,idx), D(1,idx)],[C(2,idx), D(2,idx)],'k-','LineWidth',2);
+        
+            plot(intersection_1(1,idx), intersection_1(2,idx),'rs','MarkerSize',10,'LineWidth',3);
+            plot(intersection_1(1,idx), intersection_1(2,idx),'go','MarkerSize',10,'LineWidth',3);
+            plot(intersection_2(1,idx), intersection_2(2,idx),'rs','MarkerSize',10,'LineWidth',3);
+            plot(intersection_2(1,idx), intersection_2(2,idx),'go','MarkerSize',10,'LineWidth',3);
+            
+        axis equal; 
+        title(['intersection: ' num2str(idx)]);
+        xlabel('x')
+        ylabel('y')
+    end
+end
+
+
--- script1.m
+++ script1.m
@@ -1,21 +1,20 @@
-
-      
-      str = 'images/westerncelars.JPG'
-      pic = imread(str);
+%%
+    % Load an image into Matlab
+      pic = imread('westerncelars.JPG');
       imshow(pic);
         
+ %%   user input points  
       
-      
-      info = imfinfo(str);
+      info = imfinfo('westerncelars.JPG');
       info.DigitalCamera;
-      f=info.DigitalCamera. FocalLengthIn35mmFilm;
- 
-      
-     
+      f=info.DigitalCamera.FocalLengthIn35mmFilm;
+    % Define the number of times you want to repeat the process
+    % e.g. if there are multiple lines / shapes that you need to fit to
+      num_input = 1; 
       clearvars c_info
-   
+      for j = 1:num_input
       % select n number of data points using the data cursor - in this case
-      % 4 (points ABCD)
+      % 4
           for i = 1:4
               shg
               dcm_obj = datacursormode(1);
@@ -24,18 +23,82 @@
               waitforbuttonpress
               c_info{i,j} = getCursorInfo(dcm_obj);
           end
-      
+      end
+ %%     
     % Export the positions obtained by the data cursor to the workspace
-      
+      for j = 1:num_input
           for i = 1:size(c_info,1)
               positions{i,j} = c_info{i,j}.Position;
-         
+          end
       end
-     
+      for jj =1:4
+          positions2(jj,:) = c_info{jj}.Position;
+      end
+      %%
+      for i = 1:num_input
+          % some code that fits your spline function to the data 
+      end
       close all
       
-    
+%%
 
     
+     
+% Get the equation of the line
+   xA = c_info{1, 1}.Position(1,1);
+   yA = c_info{1, 1}.Position(1,2);
+   xB = c_info{2, 1}.Position(1,1);
+   yB = c_info{2, 1}.Position(1,2);
+   xC = c_info{3, 1}.Position(1,1);
+   yC = c_info{3, 1}.Position(1,2);
+   xD = c_info{4, 1}.Position(1,1);
+   yD = c_info{4, 1}.Position(1,2);
+
+  
+   aP=positions2(1,:);
+   bP=positions2(2,:);
+   cP=positions2(3,:);
+   dP=positions2(4,:);
+   
+   AO=([xB-xA;yB-yA]);   %changement de coordonnées 
+   BO=([xB-xB;yB-yB]);   %origine B
+   CO=([xC-xB;yB-yC]);
+   DO=([xD-xB;yB-yD]);
+   
+   
+   %%
+   xO=size(pic,1)/2;
+   yO=size(pic,2)/2;
+   
+   s=1;     % car les ( skew ) pixels sont carres
     
+   K= [f,s,xO;
+       0,f,yO;
+       0,0,1]
+   %%
+   positions2(:,3)=1;
+   q = K^-1 * positions2';
+   a = q(:,1);
+   b = q(:,2);
+   c = q(:,3);
+   d = q(:,4);
+   
+   %%
+   v1 = cross(cross(a,b),cross(c,d));
+   v2 = cross(cross(a,d),cross(b,c));
+   
+   v1 = v1/v1(3);
+   v2 = v2/v2(3);
+   
+  
+   
+   %%
+   intersection_1 = lineIntersection(a(1:2),b(1:2),c(1:2),d(1:2));  %E
+   intersection_2 = lineIntersection2(a(1:2),b(1:2),c(1:2),d(1:2));  %E2
+   
+   %%
+   
+   
+   
+   
  
