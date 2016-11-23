0900b2dc96c2b663e78319e4742c8ec5666c795b
Mauricio DIAZ <mauricio.diaz@inria.fr>
2016-11-23 16:16:26 +0100

    Correction de signe dans la fonction

--- calculPose.m
+++ calculPose.m
@@ -10,19 +10,19 @@ matAux = [ -1  0  0  0  0;
            0   0 -1  0  1];
 
 ap_m = [ 0      -ap(3)  ap(2);
-        ap(3)   0       ap(1);
+        ap(3)   0       -ap(1);
        -ap(2)   ap(1)   0];
    
 bp_m = [ 0      -bp(3)  bp(2);
-        bp(3)   0       bp(1);
+        bp(3)   0       -bp(1);
        -bp(2)   bp(1)   0];
    
 cp_m = [ 0      -cp(3)  cp(2);
-        cp(3)   0       cp(1);
+        cp(3)   0       -cp(1);
        -cp(2)   cp(1)   0];
 
-dp_m = [ 0      dp(3)  dp(2);
-        dp(3)   0       dp(1);
+dp_m = [ 0      -dp(3)  dp(2);
+        dp(3)   0       -dp(1);
        -dp(2)   dp(1)   0];
 
 A = [ ap_m * matAux;
