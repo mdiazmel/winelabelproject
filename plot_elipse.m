135136846ded6c4c1f712d268c7bd38be695db35
Mauricio DIAZ <mauricio.diaz@inria.fr>
2017-02-15 17:26:31 +0100

    Ajoute : la function de verification plot_elipse dessine les ellipses convexes et concaves en ajoutant un parametre pendant l'appel

--- plot_elipse.m
+++ plot_elipse.m
@@ -1,5 +1,16 @@
-function  plot_elipse(C, corners, K, step)
+function  plot_elipse(C, corners, K, step, flag)
 
+caseM = 0;
+if (strcmp(flag,'major'))
+    caseM = 1;
+else
+    if(strcmp(flag,'minor'))
+        caseM = 0;
+    else
+        display('Bad flag, verify function call')
+    end
+end
+    
 
 xu=round(corners(1,1)):round(corners(2,1)); %Upper line
 mu=(round(corners(2,2))-round(corners(1,2)))/(round(corners(2,1))-round(corners(1,1)));
@@ -13,8 +24,11 @@ for i=1:step:length(pntu)
     pnt1=K^-1*pnt;
     rps=solve([pnt1(1,1) sym('y') 1]*C*[pnt1(1,1); sym('y'); 1]);
     pntellipse(1,1)=pnt1(1,1);
-    pntellipse(2,1)=double(rps(1));     % Arc superieur
-    %pntellipse(2,1)=double(rps(2));     % Arc inferieur
+    if (caseM)
+       pntellipse(2,1)=double(rps(1));     % Arc superieur
+    else
+       pntellipse(2,1)=double(rps(2));     % Arc inferieur
+    end
     pntellipse(3,1)=pnt1(3,1);
     pntellipse=K*pntellipse;
     points(i,:)=pntellipse(1:2,1);
